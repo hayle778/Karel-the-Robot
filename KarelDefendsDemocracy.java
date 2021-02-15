4086022c40490932ab71a6b5ae4620c15688bae6
yourUsername <yourEmail@domain.com>
2021-02-15 00:13:12 -0500

    FF

---
+++ KarelDefendsDemocracy.java
@@ -0,0 +1,26 @@
+package week1;
+import stanford.karel.*;
+
+public class KarelDefendsDemocracy extends SuperKarel {
+
+	
+	
+public void run() {
+	
+	move();
+	//putBeeper();
+   
+	//move();
+	turnLeft();
+     putBeeper();
+	//putBeeper();
+	turnRight();
+	//turnLeft();
+	
+	
+}	
+	
+
+
+
+}
